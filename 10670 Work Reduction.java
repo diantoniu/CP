6d2b5291d778e21bce1c1d3ec520980201fd5061
diantoniu <45740716+diantoniu@users.noreply.github.com>
2024-11-26 11:58:20 -0500

    10670 Work Reduction.java

--- 10670 Work Reduction.java
+++ 10670 Work Reduction.java
@@ -1,3 +1,6 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
@@ -8,39 +11,37 @@ public class Main {
     while (T > 0) {
       T--;
       caseNo++;
-      long N = sc.nextLong(), M = sc.nextLong();
-      int L = sc.nextInt();
+      int N = sc.nextInt(), M = sc.nextInt(), L = sc.nextInt();
       String[][] result = new String[L][2];
       for (int i = 0; i < L; i++) {
         String agency = sc.next();
         String agencyName = agency.split(":")[0];
         String agencyPrices = agency.split(":")[1];
-        long agencyPriceA = Integer.parseInt(agencyPrices.split(",")[0]);
-        long agencyPriceB = Integer.parseInt(agencyPrices.split(",")[1]);
+        int a = Integer.parseInt(agencyPrices.split(",")[0]);
+        int b = Integer.parseInt(agencyPrices.split(",")[1]);
         result[i][0] = agencyName;
-        long m = N;
-        long currentResult = 0;
-        while (m != M) {
-          long half = m / 2;
-          long currentMTarget = Math.max(half, M);
-          long aPriceToMTarget = (m - currentMTarget) * agencyPriceA;
-          long bPriceToMTarget = (half == currentMTarget) ? agencyPriceB : aPriceToMTarget;
-          System.out.println(half + " " + currentMTarget + " " + aPriceToMTarget + " " + bPriceToMTarget) ;
-          currentResult += Math.min(aPriceToMTarget, bPriceToMTarget);
-          m = currentMTarget;
+        int res = 0;
+        int n = N;
+        while (n != M) {
+          int half = n / 2;
+          int currentMTarget = Math.max(half, M);
+          int aPriceToMTarget = (n - currentMTarget) * a;
+          int bPriceToMTarget = (half == currentMTarget) ? b : aPriceToMTarget;
+          res += Math.min(aPriceToMTarget, bPriceToMTarget);
+          n = currentMTarget;
         }
-        result[i][1] = String.valueOf(currentResult);
+        result[i][1] = String.valueOf(res);
       }
 
       Arrays.sort(result, ((o1, o2) -> {
-        long p1 = Long.parseLong(o1[1]), p2 = Long.parseLong(o2[1]);
+        int p1 = Integer.parseInt(o1[1]), p2 = Integer.parseInt(o2[1]);
         if (p1 != p2) {
-          return Long.compare(p1, p2);
+          return p1 - p2;
         }
         return o1[0].compareTo(o2[0]);
       }));
 
-      System.out.println("Case " + caseNo + ":");
+      System.out.println("Case " + caseNo);
       for (int i = 0; i < L; i++) {
         String agencyName = result[i][0];
         String price = result[i][1];
